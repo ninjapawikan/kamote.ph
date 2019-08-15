b97c093c76c735bd62abd8ce1293f682c8b2849f
vince0003 <vince0003linkmalloc@gmail.com>
2019-08-15 17:58:22 +0800

    updated kamote

--- xterme-admin/dist/js/selectuserinfo.js
+++ xterme-admin/dist/js/selectuserinfo.js
@@ -3,15 +3,29 @@ $(document).ready(function () {
     var db = firebase.firestore(); // database
     db.collection("users").onSnapshot(snapshot => {
         snapshot.docChanges().forEach((changed) => {
+
+            console.log(changed);
+
             if (changed.type == 'added') {
-               
+               var id = changed.doc.id;
                 var data = changed.doc.data();
                 $('#userlist').prepend(`<tr>
                 <th scope="row">${data.name}</th>
                 <td>${data.car}</td>
                 <td>${data.color}</td>
                 <td>${data.overall_rating}</td>
+                <td><button data-toggle="modal" data-target="#updateinfo" class="btn btn-info" id="updateinfodata">Update Info</button> | <button data-toggle="modal" data-target="#deleteinfo" class="btn btn-danger" id="deleteinfodata">Delete Info</button></td>
             </tr>`);
+
+           // update modal set value
+            $('#updateinfodata').click(function() {
+                $('input#updatename').val(data.name);
+                $('input#updatecar').val(data.car);
+                $('input#updatecolor').val(data.color);
+                $('input#updateoverallrating').val(data.overall_rating); 
+                $('input#docid').val(id); 
+              });
+              //end update
             }
             // else if( changed.type == 'removed' ){
             //     var idToDelete = changed.doc.id;
@@ -20,4 +34,26 @@ $(document).ready(function () {
 
         });
     });
+
+    $('#updateuser').click(function(){
+
+        var name = $('input#updatename').val();
+        var car = $('input#updatecar').val();
+        var color = $('input#updatecolor').val();
+        var rating = $('input#updateoverallrating').val(); 
+        var docid = $('input#docid').val(); 
+
+        db.collection('users').doc(docid).update({
+            color:color,
+            name:name,
+            car:car,
+            overall_rating:rating
+        })    
+        .then(function (docRef) {
+           // console.log("Document written with ID: ", docRef);
+            location.reload();   
+        })
+    });
+    
+
 });
