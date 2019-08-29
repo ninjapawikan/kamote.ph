b755d9859e0e3b7d687096abc389eaf63f06fee5
vince0003 <vince0003linkmalloc@gmail.com>
2019-08-29 17:05:47 +0800

    login and signup

--- xterme-admin/login/js/main.js
+++ xterme-admin/login/js/main.js
@@ -1,35 +1,36 @@
-
-  //add realtime listener
-  var globaluserid;
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-   //      debugger;
-      //console.log(firebaseUser);
+//add realtime listener
+var globaluserid;
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    //      debugger;
+    //console.log(firebaseUser);
 
     //console.log(globaluserid);
 
 
-      if (firebaseUser) {
-        console.log(firebaseUser);
-       
+    if (firebaseUser) {
 
+        console.log(firebaseUser);
         if ((window.location.href.indexOf('login') > 0 || window.location.href.indexOf('signup') > 0)) {
             window.location = "/users.html";
         }
-      } else {
-          debugger;
-          console.log('not logged in');
-          if (window.location.href.indexOf('login') < 0) { // page is not in login page
-          window.location = "/login/";
-      }
-      }
-  });
-$(document).ready(function(){
- 
+
+        document.getElementById('emailaddress').innerHTML = "" + firebaseUser.email;
+
+    } else {
+        //   debugger;
+        console.log('not logged in');
+        if (window.location.href.indexOf('login') < 0) { // page is not in login page
+            window.location = "/login/";
+        }
+    }
+});
+$(document).ready(function () {
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('#loginbtn').on('click', function(e) {
+    $('#loginbtn').on('click', function (e) {
         e.preventDefault();
         var check = true;
 
@@ -39,17 +40,49 @@ $(document).ready(function(){
                 check = false;
             }
         }
-       // return check;
-       // alert(check);
+        // return check;
+        // alert(check);
 
-       if(check == true){
-        const email = $('#inputEmail').val();
-        const password = $('#inputPassword').val();
-        const auth = firebase.auth();
-        const promise = auth.signInWithEmailAndPassword(email, password);
-        //  console.log(promise.catch(e => e.message));
-       }
-     
+        if (check == true) {
+            const email = $('#inputEmail').val();
+            const password = $('#inputPassword').val();
+            const auth = firebase.auth();
+            const promise = auth.signInWithEmailAndPassword(email, password);
+            //  console.log(promise.catch(e => e.message));
+            promise.catch(e => toastr.error(e.message, 'Error'));
+        }
+
+    });
+
+    $('#signupbtn').on('click', function (e) {
+        e.preventDefault();
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+        // return check;
+        // alert(check);
+
+        if (check == true) {
+            const signemail = $('#signEmail').val();
+            const signpassword = $('#signPassword').val();
+            const signconpassword = $('#signConPassword').val();
+
+            if (signpassword == signconpassword) {
+                const auth = firebase.auth();
+                const promise = auth.createUserWithEmailAndPassword(signemail, signpassword);
+                promise.catch(e => toastr.error(e.message, 'Error'));
+                //window.location = "/seller/login/";
+            } else {
+                toastr.error('Password Is Not Match', 'Error');
+            }
+
+        }
+
     });
 
 
@@ -59,8 +92,8 @@ $(document).ready(function(){
         window.location.href = "/login/";
     });
 
-    $('.validate-form .input100').each(function() {
-        $(this).focus(function() {
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
             hideValidate(this);
         });
     });
